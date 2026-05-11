bc0088697821c479708fcc0a4caf501987c33e2a
mobay-store <fneves@estudantes.unisced.edu.mz>
2026-05-12 01:29:22 +0300

    Update product.py

--- routes/product.py
+++ routes/product.py
@@ -13,8 +13,8 @@ import io
 
 cloudinary.config(
     cloud_name=os.environ.get("CLOUD_NAME"),
-    api_key=os.environ.get("API_KEY"),
-    api_secret=os.environ.get("API_SECRET")
+    api_key="243372635448879",
+    api_secret="zdabFGmgS3xQ7GQK055XDs2-vXg"
 )
 
 
