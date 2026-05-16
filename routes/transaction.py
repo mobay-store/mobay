0ab5bc7e81d812fdf89543701b2639870fa860df
mobay-store <fneves@estudantes.unisced.edu.mz>
2026-05-16 08:41:20 -0400

    Update transaction.py

--- routes/transaction.py
+++ routes/transaction.py
@@ -19,24 +19,63 @@ def transactions():
     return render_template("transactions.html", transactions = transactions)
 
 
-@transaction_bp.route("/confirm_receipt", methods = ["POST","GET"])
+@transaction_bp.route("/confirm_receipt", methods=["POST", "GET"])
 def confirm_receipt():
 
     if not session.get("logged"):
         return redirect("/login")
+
     product_id = request.form.get("product_id")
+
     transaction_id = request.form.get("transaction_id")
+
     pin = request.form.get("pin")
 
     user = User.query.get_or_404(session.get("user_id"))
+
     if user.pin != pin:
+
         flash("PIN incorrecto")
+
         return redirect("/transactions")
-        
-    transaction = Transaction.query.filter_by(id=int(transaction_id)).first()
-    product = Product.query.filter_by(id=int(product_id)).first()
+
+    transaction = Transaction.query.filter_by(
+        id=int(transaction_id)
+    ).first()
+
+    product = Product.query.filter_by(
+        id=int(product_id)
+    ).first()
+
     transaction.status = "confirmada"
+
     product.status = "inactivo"
+
     db.session.commit()
+
+    # -------------------------
+    # EMAIL
+    # -------------------------
+
+    current_app.send_email(
+
+        "Pagamento confirmado",
+
+        f"""
+Pagamento confirmado.
+
+Transaction ID:
+{transaction.id}
+
+Produto:
+{product.title}
+
+Produto ID:
+{product.id}
+
+Status:
+{transaction.status}
+"""
+    )
 
     return redirect("/transactions")
