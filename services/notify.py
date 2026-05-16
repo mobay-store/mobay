c4f4d86425ca140f66b54fa97483eb64a9840abc
mobay-store <fneves@estudantes.unisced.edu.mz>
2026-05-16 10:17:19 -0400

    Update notify.py

--- services/notify.py
+++ services/notify.py
@@ -7,7 +7,7 @@ resend.api_key = os.getenv("RESEND_API_KEY")
 def notify(message):
     try:
         resend.Emails.send({
-            "from": "Meu Site mobay",
+            "from": "Mobay <onboarding@resend.dev",
             "to": os.getenv("ADMIN_EMAIL"),
             "subject": "Notificação do site",
             "text": message
